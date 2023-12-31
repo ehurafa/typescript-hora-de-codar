923629a382687c965678ae1a34527ef4cdb795d9
Rafael Gomes <rafarfl2010@gmail.com>
2023-12-31 01:24:14 -0300

    (fundamentals): numbers

---
+++ 2_FUNDAMENTALS/index.js
@@ -0,0 +1,9 @@
+// 1 - numbers
+var x = 10;
+console.log(x);
+x = 16;
+console.log(typeof x);
+var y = 15.5844545;
+console.log(typeof y);
+console.log(y);
+console.log(y.toExponential(3));
---
+++ 2_FUNDAMENTALS/index.ts
@@ -0,0 +1,16 @@
+// 1 - numbers
+let x: number = 10
+
+console.log(x)
+
+x = 16
+
+console.log(typeof x)
+
+const y: number = 15.5844545
+
+console.log(typeof y)
+
+console.log(y)
+
+console.log(y.toExponential(3))
