81173f4c8472eb4bc3d97653a4b6ca25db1ee616
Rafael Gomes <rafarfl2010@gmail.com>
2024-01-04 22:30:48 -0300

    (advanced types): Arrays

---
+++ 3_ADVANCED_TYPES/index.js
@@ -0,0 +1,8 @@
+"use strict";
+// 1 - Arrays
+let numbers = [1, 2, 4, 5];
+numbers.push(6);
+console.log(numbers[2]);
+const names = ["Rafael", "William"];
+names.push('Enzo');
+console.log(names);
---
+++ 3_ADVANCED_TYPES/index.ts
@@ -0,0 +1,12 @@
+// 1 - Arrays
+let numbers: number[] = [1, 2, 4, 5]
+
+numbers.push(6)
+
+console.log(numbers[2])
+
+const names: string[] = ["Rafael", "William"]
+
+names.push('Enzo')
+
+console.log(names)
